86b6ba6591112d92fb275189f924adf42f5d21f9
KanadeSisido <takasuyari@gmail.com>
2024-02-24 11:51:58 +0900

    logout実装

--- static/js/headerHBG.js
+++ static/js/headerHBG.js
@@ -1,3 +1,7 @@
+import {db, app} from "./importFirebase.js";
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, onAuthStateChanged, signOut,updateProfile} from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-auth.js'
+const auth = getAuth(app);
+
 let nav = document.querySelector("#navArea");
 let btn = document.querySelector(".toggle-btn");
 let mask = document.querySelector("#mask");
@@ -9,3 +13,14 @@ btn.onclick = () => {
 mask.onclick = () => {
     nav.classList.toggle("open");
 }
+
+
+function logout()
+{
+    console.log("logout")
+    signOut(auth).then(()=>{
+        window.location.href = '../selectCircle.html';    
+    });
+}
+
+window.logout = logout;
